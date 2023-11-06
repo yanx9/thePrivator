6198ace0c4b24935480511bc79b2179e8cf5c8ba
Kacper Jan Gorlowski <01159000@pw.edu.pl>
2023-11-06 23:33:10 +0100

    Main screen 30% done

---
+++ UI/main.py
@@ -0,0 +1,27 @@
+from typing import Optional, Tuple, Union
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry(f"{1100}x{700}")
+        self.title("thePrivator")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(10, weight=1)
+
+        self.top_frame = customtkinter.CTkFrame(self, height=40)
+        self.top_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="new")
+        self.searchBar = customtkinter.CTkEntry(self.top_frame, height=30, placeholder_text="Search", border_width=0)
+        self.searchBar.grid(row=0, column=0, padx=10, pady=(10, 10), sticky="w")
+        self.createButton = customtkinter.CTkButton(self.top_frame, width=30, height=30, text="+")
+        self.createButton.grid(row=0, column=1, padx=10, pady=(10, 10), sticky="w")
+        self.profile_frames = []
+        for i in range(0,10):
+            self.profile_frames.append(customtkinter.CTkFrame(self, height=50))
+            self.profile_frames[i].grid(row=i+1, column=0, padx=10, pady=(10, 0), sticky="new")
+            
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
