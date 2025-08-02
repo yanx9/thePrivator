32fd33eed30279999432b08e638caadf1203244e
Kacper Gorlowski <45717872+yanx9@users.noreply.github.com>
2025-08-02 02:24:56 +0200

    Fix package installation

--- Model/Profile.py
+++ Model/Profile.py
@@ -1,22 +1,26 @@
+from dataclasses import dataclass, asdict
 import json
-from utils import get_random_ua
+
+
+@dataclass
 class Profile:
-    def __init__(self, rc_port, name="New", chromium_version="116", user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",
-                  proxy_flag=0, proxy_url="", proxy_user="", proxy_pass="",
-                  auth_flag=0, proxy_port=""):
-        self.name = name
-        self.chromium_version = chromium_version
-        self.user_agent = user_agent
-        self.proxy_flag = proxy_flag
-        self.proxy_url = proxy_url
-        self.proxy_user = proxy_user
-        self.proxy_pass = proxy_pass
-        self.auth_flag = auth_flag
-        self.proxy_port = proxy_port
-        self.rc_port = rc_port
-        
-    def dump_config(self):
-        return json.dumps(self.__dict__, indent = 4)
+    rc_port: int
+    name: str = "New"
+    chromium_version: str = "116"
+    user_agent: str = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"
+    )
+    proxy_flag: int = 0
+    proxy_url: str = ""
+    proxy_user: str = ""
+    proxy_pass: str = ""
+    auth_flag: int = 0
+    proxy_port: str = ""
+
+    def dump_config(self) -> str:
+        """Return profile data as formatted JSON string."""
+        return json.dumps(asdict(self), indent=4)
     
 # if __name__ == "__main__":
 #     profile = Profile(
--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name='Profile', packages=find_packages())
-setup(name='UI', packages=find_packages())
-setup(name='Test', packages=find_packages())
+setup(
+    name="thePrivator",
+    version="0.1.0",
+    packages=find_packages(include=["Model", "UI", "Tests"]),
+    py_modules=["core", "main", "utils", "chromiumdownloader"],
+    install_requires=[
+        "customtkinter==5.2.1",
+        "psutil==5.9.6",
+        "setuptools",
+        "packaging",
+    ],
+    entry_points={
+        "console_scripts": [
+            "theprivator=main:main",
+        ]
+    },
+)
